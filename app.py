e9e7f6ab8c360a74805d467e2bf537a7a4aa7200
federics-up <federicasoldo747@gmail.com>
2026-07-09 11:07:14 +0200

    Fix formatting of download button in app.py

--- app.py
+++ app.py
@@ -235,4 +235,4 @@ if file_caricato is not None:
             st.download_button(
                 label="📁 Scarica Tabella Normalizzata in Excel",
                 data=excel_data,
-                file_name="Alfredo_Report_Cleaned.xlsx"
+                file_name="Alfredo_Report_Cleaned.xlsx")
